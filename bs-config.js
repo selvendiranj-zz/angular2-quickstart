2377c47f7f8f1e3236226ee5ee78de618770cf04
selvendiranj <selvendiranj@gmail.com>
2017-03-09 19:53:24 +0530

    browser config changes

--- bs-config.js
+++ bs-config.js
@@ -1,3 +1,6 @@
+'use strict';
+var fallback = require('connect-history-api-fallback');
+var log = require('connect-logger');
 /*
  |--------------------------------------------------------------------------
  | Browser-sync config file
@@ -12,68 +15,76 @@
  |
  */
 module.exports = {
-    "ui": {
-        "port": 3001,
-        "weinre": {
-            "port": 8080
+    ui: {
+        port: 3001,
+        weinre: {
+            port: 8080
         }
     },
-    "files": false,
-    "watchEvents": [
-        "change"
+    files: ['./**/*.{html,htm,css,js}'],
+    watchEvents: [
+        'change'
     ],
-    "watchOptions": {
-        "ignoreInitial": true
+    watchOptions: {
+        ignoreInitial: true,
+        ignored: 'node_modules'
     },
-    "server": {
-        "baseDir": "src",
-        "routes": {
-            "/node_modules": "node_modules"
+    server: {
+        baseDir: 'src',
+        routes: {
+            "/node_modules": 'node_modules'
+        },
+        middleware: [
+            log({ format: '%date %status %method %url' }),
+            fallback({
+                index: '/index.html',
+                htmlAcceptHeaders: ['text/html', 'application/xhtml+xml'] // systemjs workaround
+            })
+        ]
+    },
+    proxy: false,
+    port: 3000,
+    middleware: false,
+    serveStatic: [],
+    ghostMode: {
+        clicks: true,
+        scroll: true,
+        forms: {
+            submit: true,
+            inputs: true,
+            toggles: true
         }
     },
-    "proxy": false,
-    "port": 3000,
-    "middleware": false,
-    "serveStatic": [],
-    "ghostMode": {
-        "clicks": true,
-        "scroll": true,
-        "forms": {
-            "submit": true,
-            "inputs": true,
-            "toggles": true
-        }
-    },
-    "logLevel": "info",
-    "logPrefix": "BS",
-    "logConnections": false,
-    "logFileChanges": true,
-    "logSnippet": true,
-    "rewriteRules": [],
-    "open": "local",
-    "browser": "default",
-    "cors": false,
-    "xip": false,
-    "hostnameSuffix": false,
-    "reloadOnRestart": false,
-    "notify": true,
-    "scrollProportionally": true,
-    "scrollThrottle": 0,
-    "scrollRestoreTechnique": "window.name",
-    "scrollElements": [],
-    "scrollElementMapping": [],
-    "reloadDelay": 0,
-    "reloadDebounce": 0,
-    "reloadThrottle": 0,
-    "plugins": [],
-    "injectChanges": true,
-    "startPath": null,
-    "minify": true,
-    "host": null,
-    "localOnly": false,
-    "codeSync": true,
-    "timestamps": true,
-    "clientEvents": [
+    logLevel: 'info',
+    logPrefix: 'BS',
+    logConnections: false,
+    logFileChanges: true,
+    logSnippet: true,
+    rewriteRules: [],
+    open: 'local',
+    browser: 'default',
+    cors: false,
+    xip: false,
+    hostnameSuffix: false,
+    reloadOnRestart: false,
+    notify: true,
+    scrollProportionally: true,
+    scrollThrottle: 0,
+    scrollRestoreTechnique: 'window.name',
+    scrollElements: [],
+    scrollElementMapping: [],
+    reloadDelay: 0,
+    reloadDebounce: 0,
+    reloadThrottle: 0,
+    plugins: [],
+    injectChanges: true,
+    startPath: null,
+    minify: true,
+    host: null,
+    localOnly: false,
+    codeSync: true,
+    timestamps: true,
+    clientEvents: [
         "scroll",
         "scroll:element",
         "input:text",
@@ -82,29 +93,29 @@ module.exports = {
         "form:reset",
         "click"
     ],
-    "socket": {
-        "socketIoOptions": {
-            "log": false
+    socket: {
+        socketIoOptions: {
+            log: true
         },
-        "socketIoClientConfig": {
-            "reconnectionAttempts": 50
+        socketIoClientConfig: {
+            reconnectionAttempts: 50
         },
-        "path": "/browser-sync/socket.io",
-        "clientPath": "/browser-sync",
-        "namespace": "/browser-sync",
-        "clients": {
-            "heartbeatTimeout": 5000
+        path: '/browser-sync/socket.io',
+        clientPath: '/browser-sync',
+        namespace: '/browser-sync',
+        clients: {
+            heartbeatTimeout: 5000
         }
     },
-    "tagNames": {
-        "less": "link",
-        "scss": "link",
-        "css": "link",
-        "jpg": "img",
-        "jpeg": "img",
-        "png": "img",
-        "svg": "img",
-        "gif": "img",
-        "js": "script"
+    tagNames: {
+        less: 'link',
+        scss: 'link',
+        css: 'link',
+        jpg: 'img',
+        jpeg: 'img',
+        png: 'img',
+        svg: 'img',
+        gif: 'img',
+        js: 'script'
     }
 };
